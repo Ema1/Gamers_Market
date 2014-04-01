5e9045e102f47dd167fe3e058f32d892fa7b50e8
Eric Ma <ema1@masonlive.gmu.edu>
2014-04-01 12:49:22 -0400

    Fixed all issues relating to site and heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,10 @@ class UsersController < ApplicationController
     end
   end
   
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
   
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
   before_save { self.email = email.downcase }
   before_create :create_remember_token
   validates :username, presence: true, length: { maximum: 50 }
@@ -8,7 +9,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
-  has_many :microposts
+ 
   
  
   def User.new_remember_token
