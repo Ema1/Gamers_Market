592dec8b9a1f8d6db01329b9278f236f7646fe93
Eric Ma <ema1@masonlive.gmu.edu>
2014-04-10 00:31:31 -0400

    removing old game files

--- app/models/game.rb
+++
@@ -1,6 +0,0 @@
-class Game < ActiveRecord::Base
-  belongs_to :user
-  default_scope -> { order('created_at DESC') }
-  validates :content, presence: true, length: { maximum: 140 }
-  validates :user_id, presence: true
-end
--- app/views/games/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @game, :game, :platform, :user_id, :created_at, :updated_at
